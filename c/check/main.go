d1ce6d115ca5690dab129124023cba6222a0ba36
Hǎiliàng Wáng <hwang.dev@gmail.com>
2014-01-22 15:30:11 +0800

    add a new tool to check the consistancy between header file and api.xml

---
+++ c/check/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	gcc "github.com/hailiang/go-gccxml"
+	"strings"
+)
+
+var (
+	apixml = flag.String("apixml", "", "Path of api.xml")
+	header = flag.String("header", "", "Path of plplot.h")
+)
+
+func main() {
+	flag.Parse()
+	if *header == "" || *apixml == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	docFuncs := ParseApiXml(*apixml, "pl")
+	h, err := gcc.Xml{*header}.Doc()
+	c(err)
+
+	for _, hf := range h.Functions {
+		if !strings.HasPrefix(hf.CName(), "c_") {
+			continue
+		}
+		if df := docFuncs.Find(trimPrefix(hf.CName(), "c_")); df != nil {
+		} else {
+			p(hf.CName(), "is not documented.")
+		}
+	}
+
+	for _, df := range docFuncs {
+		if hf := findHeaderFunc(h, df.Name); hf != nil {
+			if len(hf.Arguments) != len(df.Args) {
+				p("parameter count mismatch for function", df.Name)
+				break
+			}
+			for i := range df.Args {
+				da, ha := df.Args[i], hf.Arguments[i]
+				if da.Name != ha.CName() {
+					p("number", i, "parameter name mismatch for function",
+						df.Name, "(", da.Name, ha.CName(), ")")
+				}
+			}
+		} else {
+			p(df.Name, "does not exist but remains in doc.")
+		}
+	}
+}
+
+func findHeaderFunc(h *gcc.XmlDoc, name string) *gcc.Function {
+	for _, f := range h.Functions {
+		if name == trimPrefix(f.CName(), "c_") {
+			return f
+		}
+	}
+	return nil
+}
+
+func trimPrefix(s, prefix string) string {
+	return strings.TrimPrefix(s, prefix)
+}
